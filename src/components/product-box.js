9da742e2419cee7815948665fdb0075fcde224eb
Evan Bonertz <evan.bonertz@commercetools.com>
2017-04-13 17:49:17 -0400

    Seperate comment and product components and export

---
+++ src/components/product-box.js
@@ -0,0 +1,47 @@
+export default class ProductBox extends React.Component {
+
+  constructor() {
+    this.state = {
+      showProducts: true,
+      description: []
+    };
+  }
+
+  componentWillMount() {
+    this._fetchProducts ();
+  }
+
+  render() {
+    const products = this.getProducts
+    return(
+      <div className="products-container">
+        <h2> The Gems </h2>
+        <div> {products.name} </div>
+      </div>
+    )
+  }
+
+  _getProducts() {
+    return this.state.comments.map((products) => {
+      return <Products
+               name={products.name.en} />
+              //  id={comment.id}
+              //  author={comment.author}
+              //  body={comment.body}
+              //  avatarUrl={comment.avatarUrl}
+              //  onDelete={this._deleteComment.bind(this)}
+              //  key={comment.id} />
+    });
+  }
+
+  _fetchProducts() {
+    jQuery.ajax({
+      method: 'GET',
+      url: 'api.sphere.io/down-under/product-projections?token=GSy9xD4iy41w1wPOR2LiHNlgbwJQG8I5',
+      headers: {Authorization: 'Bearer GSy9xD4iy41w1wPOR2LiHNlgbwJQG8I5'},
+      success: (products) => {
+        this.setState({ products })
+      }
+    });
+  }
+}
