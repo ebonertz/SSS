024d2d630dc23559d35114acf8d4595fc30f3b87
Evan Bonertz <evan.bonertz@commercetools.com>
2017-05-05 17:19:48 -0400

    Remove excess styling

--- src/app.js
+++ src/app.js
@@ -11,34 +11,21 @@ import BlogPage from './pages/blog'
 import PicturePage from './pages/picture'
 import VideoPage from './pages/video'
 
-const routes = (
-  <Router history={hashHistory}>
-    <Redirect from="/" to="/blog" />
-    <Route path="/" component={NavBar}>
-      <Route path="blog" component={BlogPage} />
-      <Route path="picture" component={PicturePage} />
-      <Route path="video" component={VideoPage} />
-    </Route>
-  </Router>
-)
-//
-// React.render(<Router>{routes}</Router>, document.getElementById('nav-bar'));
-
-// jQuery(function() {
-//   React.render(
-//     <NavBox />,
-//     document.getElementById('nav-box')
-//   );
-// })
-
+// const routes = (
+//   <Router history={hashHistory}>
+//     <Redirect from="/" to="/blog" />
+//     <Route path="/" component={NavBar}>
+//       <Route path="blog" component={BlogPage} />
+//       <Route path="picture" component={PicturePage} />
+//       <Route path="video" component={VideoPage} />
+//     </Route>
+//   </Router>
+// )
 
 jQuery(function() {
   ReactDOM.render(
     <CommentBox />,
     document.getElementById('comment-box')
-    // function() {
-    //   console.timeEnd('react-app')
-    // }
   );
 })
 
@@ -46,8 +33,5 @@ jQuery(function() {
   ReactDOM.render(
     <ProductBox />,
     document.getElementById('product-box')
-    // function() {
-    //   console.timeEnd('react-app')
-    // }
   );
 })
