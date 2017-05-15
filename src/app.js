74315bed3b680fb76c5c3c1930dae77b4ef5cbc3
Evan Bonertz <evan.bonertz@commercetools.com>
2017-05-15 17:31:29 -0400

    Add react-bootstrap and carousel component

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import jQuery from 'jquery';
 import CommentBox from './components/comment-box.js'
 import ProductBox from './components/product-box.js'
 import NavBar from'./components/nav-bar.js'
+// import Carousel from './components/carousel.js'
 
+import { Button, Alert, Carousel } from 'react-bootstrap';
 import{hashHistory, Router, Route, Redirect} from 'react-router';
 
 import BlogPage from './pages/blog'
@@ -40,5 +42,12 @@ jQuery(function() {
   ReactDOM.render(
     <NavBar />,
     document.getElementById('nav-bar')
+  );
+})
+
+jQuery(function() {
+  ReactDOM.render(
+    <Carousel />,
+    document.getElementById('carousel')
   );
 })
---
+++ src/components/carousel.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import { Button, Alert, Carousel, Caption, Item } from 'react-bootstrap';
+
+export default class Carousel extends Component{
+
+  render() {
+    return (
+      <Carousel>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="../assets/bells.jpg"/>
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Big</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="../assets/bells.jpg"/>
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Dog</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img width={900} height={500} alt="900x500" src="/src/assets/bells.jpg"/>
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Status</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      );
+  }
+}
